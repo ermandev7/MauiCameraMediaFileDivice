d65d1b5281aeea455da41844a7a0bf11ded42d11
Erman Meneses Gutierres <ermanmeneses@outlook.es>
2024-05-14 22:20:56 +0200

    Agregar archivos de proyecto.

---
+++ MauiCameraMediaFileDivice/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiCameraMediaFileDivice
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MauiCameraMediaFileDivice/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiCameraMediaFileDivice
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
